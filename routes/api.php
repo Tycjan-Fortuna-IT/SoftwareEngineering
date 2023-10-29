21c0cfac64e2d23b4f8891828664a2b21a6215b0
Tycjan-Fortuna-IT <242213@edu.p.lodz.pl>
2023-10-29 12:31:23 +0000

    Routes improved -> all with prefix /api

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,10 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Auth\RegisteredUserController;
+use Laravel\Sanctum\Http\Controllers\CsrfCookieController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +18,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/sanctum/csrf-cookie', [CsrfCookieController::class, 'show'])->middleware('web')->name('sanctum.csrf-cookie');
+
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/register', [RegisteredUserController::class, 'store'])->name('register');
+Route::post('/login', [AuthenticatedSessionController::class, 'store'])->name('login');
+// Route::post('/forgot-password', [PasswordResetLinkController::class, 'store'])->name('password.email');
+// Route::post('/reset-password', [NewPasswordController::class, 'store'])->name('password.store');
+// Route::get('/verify-email/{id}/{hash}', VerifyEmailController::class)->name('verification.verify');
+// Route::post('/email/verification-notification', [EmailVerificationNotificationController::class, 'store'])->name('verification.send');
+Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])->name('logout');
